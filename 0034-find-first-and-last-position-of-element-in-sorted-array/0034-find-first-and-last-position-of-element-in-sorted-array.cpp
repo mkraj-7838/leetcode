f9d6b0f80f6360ae5794d28996c9e5e12365bbd2
Mohit Prajapati <119717966+mkraj-7838@users.noreply.github.com>
2024-08-17 18:51:14 +0530

    Time: 10 ms (17.98%), Space: 16.4 MB (27.84%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,35 +1,45 @@
 class Solution {
 public:
     int firstOcc(vector<int>& nums, int target){
-            int low=0, high=nums.size()-1, first=-1;
-		while(low<=high){
-			int mid=low+(high-low)/2;
-			if(nums[mid]==target){
-                first=mid; 
+        int low=0, high=nums.size()-1;
+        int first=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target){
+                first=mid;
                 high=mid-1;
             }
-			else if(nums[mid]>target){high=mid-1;}
-			else{low=mid+1;}
-		}
-		return first;
-    }
-    int lastOcc(vector<int>& nums, int target){
-            int low=0, high=nums.size()-1, last=-1;
-		while(low<=high){
-			int mid=low+(high-low)/2;
-			if(nums[mid]==target){
-                last=mid; 
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
                 low=mid+1;
             }
-			else if(nums[mid]>target){high=mid-1;}
-			else{low=mid+1;}
-		}
-		return last;
+        }
+        return first;
+    }
+    int lastOcc(vector<int>& nums, int target){
+        int low=0, high=nums.size()-1;
+        int last=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target){
+                last=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return last;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
         int first=firstOcc(nums, target);
+        if(first==-1) return {-1,-1};
         int last=lastOcc(nums, target);
-
         return {first, last};
     }
 };
