df2075eb6fdfc26658c55ac2c6ea54ef6d05cc8f
Mohit Prajapati <119717966+mkraj-7838@users.noreply.github.com>
2024-10-30 00:44:34 +0530

    Time: 0 ms (100%), Space: 17.6 MB (66.61%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -6,25 +6,17 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-
 public:
-    bool isMirror(TreeNode* left, TreeNode* right) {
-        if (!left && !right)
-            return true;
-        if (!left || !right)
-            return false;
-        return (left->val == right->val) &&
-               isMirror(left->left, right->right) &&
-               isMirror(left->right, right->left);
-    }
     bool isSymmetric(TreeNode* root) {
-        if (!root)
-            return true;
-        return isMirror(root->left, root->right);
+        return root==NULL || symmHelp(root->left, root->right);
+    }
+    bool symmHelp(TreeNode* left, TreeNode* right){
+        if(left==NULL || right==NULL) return left==right;
+        if(left->val!=right->val) return false;
+        return symmHelp(left->left, right->right) && symmHelp(left->right, right->left);
     }
 };
