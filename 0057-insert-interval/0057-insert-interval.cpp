6c2c643b275c4869407eafc0223dcf1402d4f7e2
Mohit Prajapati <119717966+mkraj-7838@users.noreply.github.com>
2025-01-15 01:08:56 +0530

    Time: 0 ms (100%), Space: 21.7 MB (41.6%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newIntervals) {
-        intervals.push_back(newIntervals);
-        sort(intervals.begin(), intervals.end());
-        vector<vector<int>> ans;
-        int size=intervals.size();
+    vector<vector<int>> insert(vector<vector<int>>& intervals,
+                               vector<int>& newInterval) {
+        vector<vector<int>> arr;
+        bool inserted = false;
 
-        for(int i=0; i<size; i++){
-            if(ans.empty() || intervals[i][0]>ans.back()[1])
-                ans.push_back(intervals[i]);
-            else
-                ans.back()[1]=max(ans.back()[1], intervals[i][1]);
+        for (const auto& interval : intervals) {
+            if (interval[1] < newInterval[0]) {
+                // Current interval ends before new interval starts
+                arr.push_back(interval);
+            } else if (interval[0] > newInterval[1]) {
+                // Current interval starts after new interval ends
+                if (!inserted) {
+                    arr.push_back(newInterval);
+                    inserted = true;
+                }
+                arr.push_back(interval);
+            } else {
+                // There is overlap, merge intervals
+                newInterval[0] = min(newInterval[0], interval[0]);
+                newInterval[1] = max(newInterval[1], interval[1]);
+            }
         }
-        return ans;
+
+        // Insert the new interval at the end if it hasn't been inserted yet
+        if (!inserted) {
+            arr.push_back(newInterval);
+        }
+
+        return arr;
     }
 };
