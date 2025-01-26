09894ece761bdd087ae96dadefb77eb00a256775
Mohit Prajapati <119717966+mkraj-7838@users.noreply.github.com>
2025-01-27 00:09:49 +0530

    Time: 4 ms (33.49%), Space: 7.7 MB (87.64%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        if (n == 0) return 0;
+        
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;
+            result += n & 1;
+            n >>= 1;
+        }
+
+        return result;
+    }
+};
