27e20aa7a54fb7fb263e60590f3d39fb5685b222
Mohit Prajapati <119717966+mkraj-7838@users.noreply.github.com>
2025-03-01 12:02:33 +0530

    Time: 0 ms (100%), Space: 13 MB (87.59%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
@@ -2,28 +2,21 @@ class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
         int n = nums.size();
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
-                nums[i] = nums[i] * 2;
-                nums[i + 1] = nums[i + 1] * 0;
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (i + 1 < n && nums[i] == nums[i + 1] && nums[i] != 0) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
             }
+
+            if (nums[i] != 0) {
+                if (i != j)
+                    swap(nums[i], nums[j]);
+                j++;
+            }
+            
         }
-        int l = 0, r = 0;
-        while (r < n) {
-            while (l < n && nums[l] != 0) {
-                l++;
-            }
-            r = max(r, l);
-            while (r < n && nums[r] == 0) {
-                r++;
-            }
-            if (r < n) { 
-                nums[l] = nums[r];
-                nums[r] = 0;
-                l++;
-            }
-            r++;
-        }
+
         return nums;
     }
 };
