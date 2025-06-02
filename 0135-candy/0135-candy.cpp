5c9a6f8016c2b844cc18e788b6ed6da39ae8d49c
Mohit Prajapati <119717966+mkraj-7838@users.noreply.github.com>
2025-06-02 14:23:27 +0530

    Time: 4 ms (17.74%), Space: 23.5 MB (11.14%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -8,13 +8,13 @@ public:
         // left to right
         for (int i = 1; i < n; i++) {
             if (ratings[i] > ratings[i - 1])
-                l2r[i] = l2r[i - 1] + 1;
+                l2r[i] = max(l2r[i],l2r[i - 1] + 1);
         }
 
         // right to left
         for (int i = n - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1])
-                r2l[i] = r2l[i + 1] + 1;
+                r2l[i] = max(r2l[i], r2l[i + 1] + 1);
         }
         int ans = 0;
         for (int i = 0; i < n; i++) {
