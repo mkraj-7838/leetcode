8747f29067b9e591ad3ccfe51041056069307ac6
Mohit Prajapati <119717966+mkraj-7838@users.noreply.github.com>
2025-06-22 21:58:08 +0530

    Time: 4 ms (37.03%), Space: 16.1 MB (24.25%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    void solve(string s, int open, int close, vector<string>& ans) {
-        if (open == 0 && close == 0) {
+    void solve(string s,int open, int close, vector<string>& ans){
+        if(open==0 && close==0){
             ans.push_back(s);
             return;
         }
-        if (open == close) {
-            string s1 = s;
+        if(open==close || close == 0){
+            string s1=s;
             s1.push_back('(');
-            solve(s1, open - 1, close, ans);
-        } else if (open == 0) {
-            string s1 = s;
+            solve(s1,open-1, close, ans);
+        }else if(open==0){
+            string s1=s;
             s1.push_back(')');
-            solve(s1, open, close - 1, ans);
-        } else if (close == 0) {
-            string s1 = s;
-            s1.push_back('(');
-            solve(s1, open - 1, close, ans);
-        } else {
-            string s1 = s;
-            string s2 = s;
+            solve(s1,open, close-1, ans);
+        }else{
+            string s1=s;
+            string s2=s;
             s1.push_back('(');
             s2.push_back(')');
-            solve(s1, open - 1, close, ans);
-            solve(s2, open, close - 1, ans);
-        };
+            solve(s1,open-1, close, ans);
+            solve(s2,open, close-1, ans);
+        }
+        
     }
     vector<string> generateParenthesis(int n) {
-        int open = n;
-        int close = n;
-        string s = "";
+        int open=n, close=n;
+        string s="";
         vector<string> ans;
         solve(s, open, close, ans);
         return ans;
